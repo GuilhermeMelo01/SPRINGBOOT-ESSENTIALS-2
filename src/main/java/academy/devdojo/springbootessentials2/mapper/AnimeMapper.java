ca258481e60752408342be79dc45469b494d7f19
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-02-08 13:58:47 -0300

    aula 15 do curso de springBoot

--- src/main/java/academy/devdojo/springbootessentials2/mapper/AnimeMapper.java
+++ src/main/java/academy/devdojo/springbootessentials2/mapper/AnimeMapper.java
@@ -6,6 +6,7 @@ import academy.devdojo.springbootessentials2.requests.AnimePutRequestBody;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+
 @Mapper(componentModel = "spring")
 public abstract class AnimeMapper {
     public static final AnimeMapper INSTANCE = Mappers.getMapper(AnimeMapper.class);
